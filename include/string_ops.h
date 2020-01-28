c231de29c9fa2504a135b930db6d38fe68afb2bd
kronikarz <kronikarz@gmail.com>
2020-01-28 16:22:06 +0100

    * first commit

--- include/string_ops.h
+++ include/string_ops.h
@@ -1,3 +1,7 @@
+/// Copyright 2017-2020 Ghassan.pl
+/// Usage of the works is permitted provided that this instrument is retained with
+/// the works, so that any entity that uses the works is notified of this instrument.
+/// DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
 #pragma once
 
 #include <string_view>
